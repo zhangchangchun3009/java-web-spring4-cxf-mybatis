0cf989831ae06254768fdc4a5373d78cd9c0b83e
zhangchangchun <zhangchangchun3009@163.com>
2022-03-09 16:00:37 +0800

    a simple example of how to use the 'Disruptor' framework of LMAX

---
+++ scm.common/src/main/java/pers/zcc/scm/common/util/lmax/disruptor/LongEventFactory.java
@@ -0,0 +1,12 @@
+package pers.zcc.scm.common.util.lmax.disruptor;
+
+import com.lmax.disruptor.EventFactory;
+
+public class LongEventFactory implements EventFactory<LongEvent> {
+
+    @Override
+    public LongEvent newInstance() {
+        return new LongEvent();
+    }
+
+}
